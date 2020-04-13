20b8f433f15c76240a05124711017ef3fb5d2c22
Reza <rzgholizadeh@gmail.com>
2020-04-13 17:39:26 -0600

    feat: Implement cache in mongoose exec method

--- services/cache.js
+++ services/cache.js
@@ -7,12 +7,17 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-	console.log("Going to run a query...");
-	const key = Object.assign({}, this.getQuery(), {
-		collection: this.mongooseCollection.name,
-	});
-	console.log(key);
-
-	return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function () {
+	const key = JSON.stringify(
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name,
+		})
+	);
+	const cacheValue = await client.get(key);
+	if (cacheValue) {
+		return JSON.parse(cacheValue);
+	}
+	const result = await exec.apply(this, arguments);
+	client.set(key, JSON.stringify(result));
+	return result;
 };
