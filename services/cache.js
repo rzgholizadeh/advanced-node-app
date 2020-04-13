f19ff1c02952818da2ab2f448db9da6defd20c7d
Reza <rzgholizadeh@gmail.com>
2020-04-13 17:25:14 -0600

    refactor: Patch mongoose exec method

---
+++ services/cache.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+	console.log("Going to run a query...");
+	const key = Object.assign({}, this.getQuery(), {
+		collection: this.mongooseCollection.name,
+	});
+	console.log(key);
+
+	return exec.apply(this, arguments);
+};
