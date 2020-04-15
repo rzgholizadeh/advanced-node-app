e1d72eadd0e8c88d30741d5c72092656d7429a08
Reza <rzgholizadeh@gmail.com>
2020-04-15 13:02:38 -0600

    test: Check that clicking login button start oauth

--- test/header.test.js
+++ test/header.test.js
@@ -3,17 +3,22 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 beforeEach(async () => {
-	browser = await puppeteer.launch({ headless: false });
-	page = await browser.newPage();
-	await page.goto("localhost:3000");
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-	await browser.close();
+  await browser.close();
 });
 
-test("We can launch a browser", async () => {
-	const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+test("the header has the correct text", async () => {
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  expect(text).toEqual("Blogster");
+});
 
-	expect(text).toEqual("Blogster");
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
