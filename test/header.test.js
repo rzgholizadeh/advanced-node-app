942f2efbd4eaee6d442b174dbf5780957dca97e1
Reza <rzgholizadeh@gmail.com>
2020-04-15 11:28:30 -0600

    test: Develop beforeEach for launching chromium

--- test/header.test.js
+++ test/header.test.js
@@ -1,10 +1,14 @@
 const puppeteer = require("puppeteer");
 
-test("We can launch a browser", async () => {
-	const browser = await puppeteer.launch({ headless: false });
-	const page = await browser.newPage();
-	await page.goto("localhost:3000");
+let browser, page;
 
+beforeEach(async () => {
+	browser = await puppeteer.launch({ headless: false });
+	page = await browser.newPage();
+	await page.goto("localhost:3000");
+});
+
+test("We can launch a browser", async () => {
 	const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
 
 	expect(text).toEqual("Blogster");
