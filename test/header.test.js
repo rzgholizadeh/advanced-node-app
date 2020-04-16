3c7356bc0d2994e12def526dde4b86406d3b994e
Reza <rzgholizadeh@gmail.com>
2020-04-16 13:43:50 -0600

    test: Check the header logout button after login

--- test/header.test.js
+++ test/header.test.js
@@ -3,22 +3,48 @@ const puppeteer = require("puppeteer");
 let browser, page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({ headless: false });
-  page = await browser.newPage();
-  await page.goto("localhost:3000");
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+    await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-  await browser.close();
+    await browser.close();
 });
 
 test("the header has the correct text", async () => {
-  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
-  expect(text).toEqual("Blogster");
+    const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+    expect(text).toEqual("Blogster");
 });
 
 test("clicking login starts oauth flow", async () => {
-  await page.click(".right a");
-  const url = await page.url();
-  expect(url).toMatch(/accounts\.google\.com/);
+    await page.click(".right a");
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("when signed in, shows logout button", async () => {
+    const id = "5e9262e0e5985c32b4e85d6b";
+    const buffer = require("safe-buffer").Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    };
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+        "base64"
+    );
+    const Keygrip = require("keygrip");
+    const keys = require("../config/keys");
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign("session=" + sessionString);
+    await page.setCookie({ name: "session", value: sessionString });
+    await page.setCookie({ name: "session.sig", value: sig });
+    await page.goto("localhost:3000");
+    await page.waitFor('a[href="/auth/logout"]');
+    const text = await page.$eval(
+        'a[href="/auth/logout"]',
+        (el) => el.innerHTML
+    );
+    expect(text).toEqual("Logout");
 });
