29c7a63ac51e9139b624b04b334e0d5b21075345
Reza <rzgholizadeh@gmail.com>
2020-04-18 17:53:08 -0600

    chore: Create ci file for keys

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+        "461425647812-leus577p78bvk8l1pusf0r2m272pdh9p.apps.googleusercontent.com",
+    googleClientSecret: "WsKhAn7UerlQiQ07N1dEk2_7",
+    mongoURI: "",
+    cookieKey: "123123123"
+};
