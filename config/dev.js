1517c9b61387d4aa8a1b8ea898fe14c2626b02bc
Reza <rzgholizadeh@gmail.com>
2020-04-19 15:29:23 -0600

    chore: Remove dev keys

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    googleClientID:
-        "461425647812-leus577p78bvk8l1pusf0r2m272pdh9p.apps.googleusercontent.com",
-    googleClientSecret: "WsKhAn7UerlQiQ07N1dEk2_7",
-    mongoURI:
-        "mongodb://advancedNode:28f6QsP6nQyFQ91d@cluster0-shard-00-00-kncq1.mongodb.net:27017,cluster0-shard-00-01-kncq1.mongodb.net:27017,cluster0-shard-00-02-kncq1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    cookieKey: "123123123",
-    redisUrl: "redis://127.0.0.1:6379"
-};
