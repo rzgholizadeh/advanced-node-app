66c2a5d020eedaa6cb25dd11c152008a7e481f23
Reza <rzgholizadeh@gmail.com>
2020-04-18 18:10:30 -0600

    fix: Update test config for ci

--- test/header.test.js
+++ test/header.test.js
@@ -5,25 +5,25 @@ let page;
 
 beforeEach(async () => {
     page = await Page.build();
-    await page.goto("localhost:3000");
+    await page.goto("http://localhost:3000");
 });
 
 afterEach(async () => {
     await page.close();
 });
 
-test.skip("the header has the correct text", async () => {
+test("the header has the correct text", async () => {
     const text = await page.getContentsOf("a.brand-logo");
     expect(text).toEqual("Blogster");
 });
 
-test.skip("clicking login starts oauth flow", async () => {
+test("clicking login starts oauth flow", async () => {
     await page.click(".right a");
     const url = await page.url();
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.skip("when signed in, shows logout button", async () => {
+test("when signed in, shows logout button", async () => {
     await page.login();
     const text = await page.getContentsOf('a[href="/auth/logout"]');
     expect(text).toEqual("Logout");
