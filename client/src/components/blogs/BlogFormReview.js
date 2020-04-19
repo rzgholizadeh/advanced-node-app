3e7d12d7223ccb428ed1f16c3526fda9a7bc967c
Reza <rzgholizadeh@gmail.com>
2020-04-19 14:08:32 -0600

    feat: Add file picker to blog review

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -1,66 +1,67 @@
 // BlogFormReview shows users their form inputs for review
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import formFields from './formFields';
-import { withRouter } from 'react-router-dom';
-import * as actions from '../../actions';
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
 class BlogFormReview extends Component {
-  renderFields() {
-    const { formValues } = this.props;
+    renderFields() {
+        const { formValues } = this.props;
 
-    return _.map(formFields, ({ name, label }) => {
-      return (
-        <div key={name}>
-          <label>{label}</label>
-          <div>{formValues[name]}</div>
-        </div>
-      );
-    });
-  }
+        return _.map(formFields, ({ name, label }) => {
+            return (
+                <div key={name}>
+                    <label>{label}</label>
+                    <div>{formValues[name]}</div>
+                </div>
+            );
+        });
+    }
 
-  renderButtons() {
-    const { onCancel } = this.props;
+    renderButtons() {
+        const { onCancel } = this.props;
 
-    return (
-      <div>
-        <button
-          className="yellow darken-3 white-text btn-flat"
-          onClick={onCancel}
-        >
-          Back
-        </button>
-        <button className="green btn-flat right white-text">
-          Save Blog
-          <i className="material-icons right">email</i>
-        </button>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <button
+                    className="yellow darken-3 white-text btn-flat"
+                    onClick={onCancel}
+                >
+                    Back
+                </button>
+                <button className="green btn-flat right white-text">
+                    Save Blog
+                    <i className="material-icons right">email</i>
+                </button>
+            </div>
+        );
+    }
 
-  onSubmit(event) {
-    event.preventDefault();
+    onSubmit(event) {
+        event.preventDefault();
 
-    const { submitBlog, history, formValues } = this.props;
+        const { submitBlog, history, formValues } = this.props;
 
-    submitBlog(formValues, history);
-  }
+        submitBlog(formValues, history);
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.onSubmit.bind(this)}>
-        <h5>Please confirm your entries</h5>
-        {this.renderFields()}
-
-        {this.renderButtons()}
-      </form>
-    );
-  }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <h5>Please confirm your entries</h5>
+                {this.renderFields()}
+                <h5>Add an Image</h5>
+                <input type="file" accept="image/*" />
+                {this.renderButtons()}
+            </form>
+        );
+    }
 }
 
 function mapStateToProps(state) {
-  return { formValues: state.form.blogForm.values };
+    return { formValues: state.form.blogForm.values };
 }
 
 export default connect(mapStateToProps, actions)(withRouter(BlogFormReview));
